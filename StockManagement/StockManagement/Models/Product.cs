1a78b0a1685c595cff5497a397c76f3b80cbc6c3
Mateus Fonseca <mateus102006@hotmail.com>
2025-01-05 09:45:11 -0300

    adjusts nullables

--- StockManagement/StockManagement/Models/Product.cs
+++ StockManagement/StockManagement/Models/Product.cs
@@ -5,10 +5,10 @@ namespace StockManagement.Models
     public class Product
     {
         public Guid Id { get; private set; }
-        public string Name { get; private set; }
+        public string? Name { get; private set; }
         public int StockQuantity { get; private set; }
         public decimal Price { get; private set; }
-        public string Desctription { get; private set; }
+        public string? Desctription { get; private set; }
         public Categorie Categorie { get; private set; }
 
         public Product(string name, int stockQuantity, decimal price, string description, Categorie categorie = Categorie.Other)
